edc54fc0cd50c9f7316e7e038ff2aeb627a3ab35
Yiruz <2yi.z.ru2@gmail.com>
2015-12-16 12:19:17 -0500

    create driver and start on actionPerformed

--- GUI/Window.java
+++ GUI/Window.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 public class Window extends JFrame {
     private Container pane;
+    private JTextField t;
+    private JLabel l;
 
     public Window() {
         this.setTitle("Temperture Conversion");
@@ -11,14 +13,31 @@ public class Window extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(new BoxLayout(pane,BoxLayout.PAGE_AXIS));
 
 	JButton b1 = new JButton("C to F");
 	pane.add(b1);
+	b1.addActionListener(this);
+	b1.setActionCommand("From Celcius");
 	JButton b2 = new JButton("F to C");
 	pane.add(b2);
-	JTextField t = new JTextField(10);
+	b2.addActionListener(this);
+	b2.setActionCommand("From Farenheit");
+	t = new JTextField(10);
 	pane.add(t);
+	l = new JLabel("");
+	pane.add(l);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("From Celcius")){
+	    int s = CtoF(t.getText());
+	    l.setText(s);	    
+	}
+	if(event.equals("From Farenheit")){
+	    
+	}
     }
 
     public static double CtoF(double t){
@@ -28,9 +47,5 @@ public class Window extends JFrame {
 	return (t - 32) * 5/9;
     }
 
-    public static void main(String[] args) {
-        Window g = new Window();
-        g.setVisible(true);
 
-    }
 }
