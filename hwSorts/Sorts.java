a83a8aa63b893b24fe65b10978b7dba2a0a6388d
Yiruz <2yi.z.ru2@gmail.com>
2016-01-04 23:30:13 -0500

    complete selection sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -10,7 +10,19 @@ public class Sorts{
 	    }
 	}
     }
-
+    public static void selection(int[]data){
+	for(int i=0;i<data.length;i++){
+	    int first = i;
+	    for(int j=i;j<data.length;j++){
+		if(data[i]>data[j]){
+		    first=j;
+		}
+	    }
+	    int temp = data[first];
+	    data[first]=data[i];
+	    data[i]=temp;
+	}
+    }
     public static void print(int[]data){
 	System.out.print("[");
 	for(int i=0;i<data.length-1;i++){
@@ -19,8 +31,11 @@ public class Sorts{
 	System.out.print(data[data.length-1]+"]\n");
     }
     public static void main(String[]args){
-	int[]data={2,1,8,0,22,7};
+	int[]data={2,1,8,0,22,7,-4};
+	int[]copy={2,1,8,0,22,7,-4};
 	insertion(data);
+	print(data);
+	selection(copy);
 	print(data);
     }
 }
