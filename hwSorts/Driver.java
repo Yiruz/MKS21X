7fdb9a55b652ee2a5e43290bfe6a6e9fc550f805
Yiruz <2yi.z.ru2@gmail.com>
2016-01-06 19:33:43 -0500

    fix minor problems

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -2,17 +2,15 @@ import java.util.Arrays;
 public class Driver{
     public static void main(String[]args){
         int[]data=new int[5];
-        for(int i=0;i<data.length;i++){
-            data[i]=(int)(Math.random()*100-50);
-        }
-        int[]copy=new int[5];
+        /*int[]copy=new int[5];
         for(int i=0;i<data.length;i++){
             copy[i]=data[i];
-        }
+	    }*/
+	Sorts.fillRandom(data);
         System.out.println(Arrays.toString(data));
-        Sorts.bubbleSort(data);
-        System.out.println(Arrays.toString(data));
-        Sorts.selectionSort(copy);
+        System.out.println("-----------------\nStart");
+	Sorts.bubbleSort(data);
+	//Sorts.selectionSort(data);
         System.out.println(Arrays.toString(data));
     }
 
